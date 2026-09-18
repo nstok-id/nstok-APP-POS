1a1d53ae810403b052f145afef4a5f8ac5dd454d
RynHerz <yannzzz13@gmail.com>
2026-09-19 00:57:16 +0800

    chore(config): allow cross-origin dev access for mobile LAN testing

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  experimental: {
+    // Allows accessing dev server from mobile devices (iOS/Android) on local LAN
+  },
 };
 
 module.exports = nextConfig;
